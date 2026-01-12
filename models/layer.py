52fb00749f9b7a9547f4f60ec6a1ad6e733eb03e
Chengxu <90556664+jianbaixuezhang@users.noreply.github.com>
2026-01-12 20:07:16 +0800

    Create layer.py

---
+++ models/layer.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+
+class PatchEmbed(nn.Module):
+	def __init__(self, patch_size=4, in_chans=3, embed_dim=96, kernel_size=None):
+		super().__init__()
+		self.in_chans = in_chans
+		self.embed_dim = embed_dim
+
+		if kernel_size is None:
+			kernel_size = patch_size
+
+		self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=kernel_size, stride=patch_size,
+							  padding=(kernel_size-patch_size+1)//2, padding_mode='reflect')
+
+	def forward(self, x):
+		x = self.proj(x)
+		return x
+
+
+class PatchUnEmbed(nn.Module):
+	def __init__(self, patch_size=4, out_chans=3, embed_dim=96, kernel_size=None):
+		super().__init__()
+		self.out_chans = out_chans
+		self.embed_dim = embed_dim
+
+		if kernel_size is None:
+			kernel_size = 1
+
+		self.proj = nn.Sequential(
+			nn.Conv2d(embed_dim, out_chans*patch_size**2, kernel_size=kernel_size,
+					  padding=kernel_size//2, padding_mode='reflect'),
+			nn.PixelShuffle(patch_size)
+		)
+
+	def forward(self, x):
+		x = self.proj(x)
+		return x
